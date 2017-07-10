7683ecf2700042f7a41d34a48c0249f6c45525ae
sevenquarkoniums <sevenquarkoniums@gmail.com>
2017-07-10 16:20:36 -0700

    change

--- draw.py
+++ draw.py
@@ -4,9 +4,40 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def main():
-    coreData()
+    #coreData(mode='violin', figFolder='cabNodeMetric')
+    compPhase(figFolder='cabFig')
     #ipmi()
+    #t = timeseries()
+    #t.getData('rawdata/mg_s1_p90_r2/cores_36/quartz1000/rank_0')
+    #for metric in ['frequency', 'IPC', 'PKG_POWER.0', 'Temp.00']:
+    #    t.draw(metric, 'quartz_mg.C')
 
+class timeseries():
+    def __init__(self):
+        pass
+
+    def getfolders(self, path):
+        from os import walk
+        self.folders = [x[0] for x in walk(path)][1:]
+
+    def getData(self, fname):
+        df = pd.read_csv(fname, sep='\t')
+        self.data = df[['Timestamp.g','Node','PKG_POWER.0','Temp.00']].copy()
+        self.data['IPC'] = (df['INST_RETIRED.ANY.00'].shift(-1) - df['INST_RETIRED.ANY.00']) / (df['TSC.00'].shift(-1) - df['TSC.00'])
+        self.data['frequency'] = 2.4 * (df['APERF.00'].shift(-1) - df['APERF.00']) / (df['MPERF.00'].shift(-1) - df['MPERF.00'])
+
+    def draw(self, metric, title):
+        sns.set(font_scale=4)
+        sns.set_style('white')
+        fig, ax = plt.subplots(figsize=(20,10))
+        sns.tsplot(self.data[metric])
+        plt.title(title)
+        #g.axes[0,0].set_xlim(1,1400)
+        #ax.set_ylim(89.5,89.7)
+        ax.set(xlabel='Time', ylabel=metric)
+        plt.savefig('timeseries/%s_%s.png' % (metric, title) )
+        plt.close()
+        
 def ipmi():
     df = pd.read_csv('ipmi.dat',sep=' ')
     #node = df[df['node']==2]
@@ -16,47 +47,98 @@ def ipmi():
         title = 'ipmi_phase1_set2_prime95_run1_end_%s' % metric
         drawing(oneTime, metric, title)
 
-def coreData():
-    metrics = ['Temp']
-    #metrics = ['INST_RETIRED.ANY','CPU_CLK_UNHALTED.THREAD','CPU_CLK_UNHALTED.REF_TSC','IA32_PERF_STATUS','ARITH.FPU_DIV','BR_INST_RETIRED.ALL_BRANCHES','UNC_M_CAS_COUNT.WR','UNC_M_CAS_COUNT.RD','APERF','MPERF','TSC','Temp','OPENMP.INST','OPENMP.ID']
-    files = getfiles('data/')
+def compPhase(figFolder):
+    phase1 = pd.read_csv('data/cab/processor_phase1_set1_prime95_pcap115_run1.csv')
+    phase2 = pd.read_csv('data/cab/processor_phase2_set1_prime95_pcap115_run1.csv')
+    phase1['phase'] = [1] * len(phase1)
+    phase2['phase'] = [2] * len(phase2)
+    time1 = phase1['time'].max()
+    time2 = phase2['time'].max()
+    merged = pd.concat([ phase1[phase1['time']==time1], phase2[phase2['time']==time2] ], ignore_index=True)
+    newtitle = 'compPhase_nodePower_set1_prime95_pcap115_run1'
+    #drawing(merged, figFolder, newtitle)
+    merged = addLayout(merged)
+
+    compRack(merged, figFolder, newtitle, metric='PKG_POWER')
+
+def coreData(mode, figFolder):
+    #metrics = ['Temp','IPC','IPCpW','PKG_POWER','DRAM_POWER']
+    files = getfiles('data/cab/')
     for f in files:
         level = f.split('/')[-1].split('_')[0]
+        app = f.split('/')[-1].split('_')[2]
         #phase = int(f.split('/')[-1].split('_')[1][5])
-        if level == 'processor':# and phase == 1:
-            for metric in metrics:
-                title = f.split('/')[-1].split('.csv')[0]
-                df = pd.read_csv(f)
-                times = sorted(list(set(df['time'])))
+        #if level == 'processor' and app == 'mg.C':
+        if f.split('/')[-1] == 'processor_phase2_set1_prime95_pcap115_run1.csv':
+            title = f.split('/')[-1].split('.csv')[0]
+            df = pd.read_csv(f)
+            #metrics = set(df.columns) - set(['time','exactTime','node','processor'])
+            times = sorted(list(set(df['time'])))
+            if mode == 'scatter':
                 period = 0
                 for time in times:
-                    for processor in [1,2]:
-                        newtitle = title + '_proc%d_period%d' % (processor, period)
-                        drawing(df[ (df['time']==time) & (df['processor']==processor) ], metric, newtitle)
+                    newtitle = title + '_Temp_period%d' % (period)
+                    drawing(df[ (df['time']==time) ], figFolder, newtitle)
+                    #for processor in [1,2]:
+                        #newtitle = title + '_proc%d_period%d' % (processor, period)
+                        #drawing(df[ (df['time']==time) & (df['processor']==processor) ], newtitle)
                     period += 1
+            elif mode == 'violin':
+                time = times[-1]
+                oneTime = df[ (df['time']==time) ].copy()
+                merged = addLayout(oneTime)
+
+                newtitle = title + '_Temp_compRack'
+                compRack(merged, figFolder, newtitle, 'Temp')
+
+                #for rack in range(1,24):
+                #    newtitle = title + '_rack%d' % (rack)
+                #    oneRack = merged[merged['Rack']==rack]
+                #    if len(oneRack) != 0:
+                #        compRack(oneRack, figFolder, newtitle)
     print('I created some figures!')
 
-def drawing(df, metric, title):
+def addLayout(df):
+    layout = pd.read_csv('cab_layout.csv')
+    layout.rename(columns={'Node':'node'}, inplace=True)
+    return pd.merge(df, layout, on='node')
+
+def compRack(df, folder, title, metric='Temp'):
     print(title)
     sns.set(font_scale=4)
-    sns.set_style('whitegrid', {'grid.color':'.15', 'axes.edgecolor':'.15'})
-    g = sns.pairplot(df, kind='scatter', x_vars=['IPC'], y_vars=['PKG_POWER'], 
-                    #vars=['node', 'Temp', 'PKG_POWER', 'IPC', 'frequency'], 
-                    hue='processor', #hue_order=[0, 7], 
-                    palette=sns.color_palette('Set2',10), size=20)
+    sns.set_style('white')#, {'grid.color':'.15', 'axes.edgecolor':'.15'})
+    fig, ax = plt.subplots(figsize=(20,20))
+    sns.violinplot(data=df, x='Rack', y=metric,
+                    hue='phase', #hue_order=[0, 7], 
+                    inner='box', 
+                    palette=sns.color_palette('Set2',10) )
     plt.title(title)
-    #g.axes[0,0].set_xlim(90,115)
-    #g.axes[0,0].set_ylim(45,90)
-    if 'mg.C' in title:
-        g.axes[0,0].set_xlim(0.7,1.3)
-        g.axes[0,0].set_ylim(90,115)
-    elif 'prime95' in title:
-        g.axes[0,0].set_xlim(2,2.25)
-        g.axes[0,0].set_ylim(105,115)
-    #for position in range(70, 1296, 70):
+    #g.axes[0,0].set_xlim(0,1450)
+    #g.axes[0,0].set_ylim(50,90)
+    plt.savefig('%s/%s.png' % (folder, title) )
+    plt.close()
+
+def drawing(df, folder, title, metric='Temp'):
+    print(title)
+    sns.set(font_scale=4)
+    sns.set_style('white')#, {'grid.color':'.15', 'axes.edgecolor':'.15'})
+    g = sns.pairplot(df, kind='scatter', x_vars=['Temp'], y_vars=['IPCpW'], 
+                    #vars=['node', 'Temp', 'PKG_POWER', 'IPC', 'frequency'], 
+                    hue='phase', #hue_order=[0, 7], 
+                    size=20, palette=sns.color_palette('Set2',10) )
+    plt.title(title)
+    #g.axes[0,0].set_xlim(0,1450)
+    g.axes[0,0].set_ylim(0.017,0.021)
+    #if 'mg.C' in title:
+    #    g.axes[0,0].set_xlim(0.7,1.3)
+    #    g.axes[0,0].set_ylim(90,115)
+    #elif 'prime95' in title:
+    #    g.axes[0,0].set_xlim(2,2.25)
+    #    g.axes[0,0].set_ylim(105,115)
+    #for position in list(range(70, 631, 70)) + [650,670,690,760,830,900,970,974,1044,1114,1184,1254,1296]:
     #    plt.axvline(x=position, color='r')
     #plt.legend(bbox_to_anchor=(1.02, 1), loc=2, borderaxespad=0.1)
-    g.savefig('oneProcIPCPower/%s.png' % (title) )
+    g.savefig('%s/%s.png' % (folder, title) )
     plt.close()
 
 def getfiles(path):
--- pattern.py
+++ pattern.py
@@ -3,6 +3,7 @@
 by Yijia Zhang at June 22, 2017
 
 Machine learning analysis of the data.
+Using DBSCAN clustering algorithm to cluster the 3-d data (Temp, Power, IPC).
 '''
 import time
 import pandas as pd
@@ -10,45 +11,85 @@ import pandas as pd
 def main():
     start = time.time()
 
-    for phase in [1]:
-        for setID in range(1, 2):
-            for app in ['prime95']:
-                for pcap in [115]:
-                    for runID in [1]:
-
-                        a = analysis(phase, setID, app, pcap, runID, level='processor')
-                        a.readData()
-
-                        #for (eps, min_samples) in [(0.3,20),(0.4,30),(0.5,60),(0.6,100),(0.7,160),(0.8,240)]:
-                        if a.exist:
-                            a.times = sorted(list(set(a.data['time'])))
-                            period = 0
-                            for epoch in a.times:
-                                for processor in [1]:
-
-                                    #for eps in [x/10 for x in range(1, 10)]:
-                                        #for min_samples in range(10, 101, 10):
-                                    #a.cluster(algorithm='dbscan', metrics=['Temp','PKG_POWER','IPC','IPCpW','frequency','IA32_PERF_STATUS','BR_INST_RETIRED.ALL_BRANCHES','UNC_M_CAS_COUNT.WR','UNC_M_CAS_COUNT.RD'], eps=eps, min_samples=min_samples)
-                                    #a.cluster(algorithm='dbscan', metrics=['Temp','PKG_POWER','IPC','IPCpW','frequency','INST_RETIRED.ANY','CPU_CLK_UNHALTED.THREAD','CPU_CLK_UNHALTED.REF_TSC','IA32_PERF_STATUS','ARITH.FPU_DIV','BR_INST_RETIRED.ALL_BRANCHES','UNC_M_CAS_COUNT.WR','UNC_M_CAS_COUNT.RD','APERF','MPERF','TSC','Temp','OPENMP.INST','OPENMP.ID'], eps=eps, min_samples=min_samples)
-                                    #a.data.to_csv('clusters_phase1_set2_prime95_run1.csv', index=False)
-                                    (eps, min_samples) = (0.6, 60) if processor == 1 else (0.5, 20)
-                                    a.cluster(epoch, processor, algorithm='dbscan', metrics=['Temp', 'PKG_POWER', 'IPC'], eps=eps, min_samples=min_samples)
-                                    #if a.clusters >= 2 and a.normalRatio > 0.6:
-                                    print('time=%d, proc=%d, eps=%.2f, min_samples=%d' % (epoch, processor, eps, min_samples) )
-                                    print('number of clusters: %d' % a.clusters)
-                                    print('ratio of non-noise: %.2f' % a.normalRatio )
-                                    print()
-                                    a.draw(epoch, processor, eps, min_samples)
-                                period += 1
+    #findPara()
+    compareCluster(compare='epoch')
 
     duration = time.time() - start
     print('finished in %d seconds.' % round(duration) )
 
+def findPara():
+    for phase in [1]:
+        for setID in range(1, 2):
+            for app in ['prime95']:
+                for pcap in [90]:
+                    for runID in [1]:
+                        machine = 'quartz'
+                        a = analysis(machine, phase, setID, app, pcap, runID, level='processor')
+                        a.readData()
+
+                        #for (eps, min_samples) in [(0.3,20),(0.4,30),(0.5,60),(0.6,100),(0.7,160),(0.8,240)]:
+                        if a.exist:
+                            a.times = sorted(list(set(a.data['time'])), reverse=True)
+                            period = 0
+                            epoch = a.times[0]
+                            #for epoch in a.times:
+                            for processor in [1]:
+                                #(eps, min_samples) = (0.6, 60) if processor == 1 else (0.5, 20)
+                                for eps in [x/10 for x in range(1, 10)]:
+                                    for min_samples in range(10, 101, 10):
+                                        a.cluster(epoch, processor, algorithm='dbscan', metrics=['Temp', 'PKG_POWER', 'IPC'], eps=eps, min_samples=min_samples)
+                                        #if period == 9:
+                                        #    a.selecData.to_csv('prime95_proc1_clusters.csv',index=False)
+                                        if a.clusters >= 2 and a.normalRatio > 0.85:
+                                            print('time=%d, proc=%d, eps=%.2f, min_samples=%d' % (epoch, processor, eps, min_samples) )
+                                            print('number of clusters: %d' % a.clusters)
+                                            print('ratio of non-noise: %.2f' % a.normalRatio )
+                                            print()
+                                            a.draw(99, processor, eps, min_samples)
+                            period += 1
+
+def compareCluster(compare):
+    data1 = analysis(machine='quartz', phase=1, setID=1, app='prime95', pcap=90, runID=1, level='processor')
+    data1.readData()
+    data1.epoch = sorted(list(set(data1.data['time'])), reverse=True)[0]
+    data1.cluster(data1.epoch, processor=1, algorithm='dbscan', metrics=['Temp', 'PKG_POWER', 'IPC'], eps=0.4, min_samples=10)
+
+    if compare == 'app':
+        mg = analysis(phase=1, setID=1, app='mg.C', pcap=115, runID=1, level='processor')
+        mg.readData()
+        mg.epoch = sorted(list(set(mg.data['time'])), reverse=True)[0]
+        mg.cluster(mg.epoch, processor=1, algorithm='dbscan', metrics=['Temp', 'PKG_POWER', 'IPC'], eps=0.6, min_samples=60)
+
+        mg.selecData.drop(['cluster'], axis=1, inplace=True)
+        mg.selecData = pd.merge(mg.selecData, data1.selecData[['node','cluster']], on='node')
+        mg.draw(99, 1, 0.6, 60)# draw using the cluster from prime95.
+    elif compare == 'set':
+        data2 = analysis(phase=1, setID=2, app='prime95', pcap=115, runID=1, level='processor')
+        data2.readData()
+        if len(set(data2.data['time'])) == 0:
+            return 1
+        data2.epoch = sorted(list(set(data2.data['time'])), reverse=True)[0]
+        data2.cluster(data2.epoch, processor=1, algorithm='dbscan', metrics=['Temp', 'PKG_POWER', 'IPC'], eps=0.6, min_samples=60)
+
+        data2.selecData.drop(['cluster'], axis=1, inplace=True)
+        data2.selecData = pd.merge(data2.selecData, data1.selecData[['node','cluster']], on='node')
+        data2.draw(99, 1, 0.6, 60, compSet)# draw using the cluster from prime95.
+    elif compare == 'epoch':
+        data2 = analysis(machine='quartz', phase=1, setID=1, app='prime95', pcap=90, runID=1, level='processor')
+        data2.readData()
+        epochs = sorted(list(set(data2.data['time'])))
+        for data2.epoch in epochs:
+            data2.cluster(data2.epoch, processor=1, algorithm='dbscan', metrics=['Temp', 'PKG_POWER', 'IPC'], eps=0.4, min_samples=10)
+            data2.selecData.drop(['cluster'], axis=1, inplace=True)
+            data2.selecData = pd.merge(data2.selecData, data1.selecData[['node','cluster']], on='node')
+            data2.draw(period=data2.epoch, processor=1, eps=0.4, min_samples=10)# draw using the cluster from prime95.
+
 class analysis:
     '''
-    This class is for merging the relevant data distributed in different files into a single file.
+    Pattern recognitino.
     '''
-    def __init__(self, phase, setID, app, pcap, runID, level):
+    def __init__(self, machine, phase, setID, app, pcap, runID, level):
+        self.machine = machine
         self.phase = phase
         self.setID = setID
         self.app = app
@@ -63,10 +104,17 @@ class analysis:
         print('runID=%d' % runID)
 
     def readData(self):
+        '''
+        Check file existence and read.
+        '''
         import os.path
-        self.name = '%s_phase%d_set%d_%s_pcap%d_run%d' % (self.level, self.phase, self.setID, self.app, self.pcap, self.runID)
-        if os.path.isfile('data/%s.csv' % self.name):
-            self.data = pd.read_csv('data/%s.csv' % self.name)
+        if self.machine == 'cab':
+            self.name = '%s_phase%d_set%d_%s_pcap%d_run%d' % (self.level, self.phase, self.setID, self.app, self.pcap, self.runID)
+        elif self.machine == 'quartz':
+            self.name = '%s_set%d_%s_pcap%d_run%d' % (self.level, self.setID, self.app, self.pcap, self.runID)
+
+        if os.path.isfile('data/%s/%s.csv' % (self.machine, self.name) ):
+            self.data = pd.read_csv('data/%s/%s.csv' % (self.machine, self.name) )
             self.exist = True
         else:
             print('file not exist.')
@@ -88,20 +136,20 @@ class analysis:
             self.clusters = n_clusters_
             self.normalRatio = 1 - self.selecData['cluster'].value_counts()[-1] / self.selecData['cluster'].count()
 
-    def draw(self, epoch, processor, eps, min_samples):
+    def draw(self, period, processor, eps, min_samples):
         import matplotlib.pyplot as plt
         import seaborn as sns
         sns.set(font_scale=4)
         sns.set_style('whitegrid', {'grid.color':'.15', 'axes.edgecolor':'.15'})
-        g = sns.pairplot(self.selecData, kind='scatter', x_vars=['Temp'], y_vars=['IPC'], 
+        g = sns.pairplot(self.selecData, kind='scatter', x_vars=['IPC'], y_vars=['PKG_POWER'], 
                         #vars=['node', 'processor', 'cluster'], 
                         #vars=['node','Temp','IPC','PKG_POWER'],
                         hue='cluster', #hue_order=[0, 7], 
                         palette=sns.color_palette('Set2',10), size=20)
-        plt.title('%s_proc%d_period%d_eps%.2f_samples%d' % (self.name, processor, epoch, eps, min_samples) )
-        g.axes[0,0].set_xlim(50,85)
-        g.axes[0,0].set_ylim(2,2.25)
-        g.savefig('oneProcCluster/%s_proc%d_period%d_eps%.2f_samples%d.png' % (self.name, processor, epoch, eps, min_samples) )
+        plt.title('%s_proc%d_period%d_eps%.2f_samples%d' % (self.name, processor, period, eps, min_samples) )
+        #g.axes[0,0].set_xlim(105,115)
+        #g.axes[0,0].set_ylim(2,2.25)
+        g.savefig('quartzCluster/%s_proc%d_period%d_eps%.2f_samples%d_clusterFromEnd.png' % (self.name, processor, period, eps, min_samples) )
         plt.close()
 
 if __name__ == '__main__':
